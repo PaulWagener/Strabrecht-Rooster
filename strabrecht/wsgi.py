7b83e544171dc107d566187b5af2c4803e0d05c9
Paul <mail@paulwagener.nl>
2015-06-13 00:54:16 +0200

    Add Path

--- strabrecht/wsgi.py
+++ strabrecht/wsgi.py
@@ -7,8 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "strabrecht.settings")
+
+path = '/var/www/strabrecht.paulwagener.nl'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
