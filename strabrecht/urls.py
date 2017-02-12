ebd036182484dfb8de9d08317a0e6e6e79c2e0cf
Paul Wagener <mail@paulwagener.nl>
2017-02-12 12:40:31 +0100

    Upgrade for Django

--- strabrecht/urls.py
+++ strabrecht/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
+from rooster import views
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', 'rooster.views.index'),
-    url(r'^(teacher|room|group|student)/([a-zA-Z0-9_]+)\.(ics|json)$', 'rooster.views.events'),
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^(teacher|room|group|student)/([a-zA-Z0-9_]+)\.(ics|json)$', views.events),
     url(r'^admin/', include(admin.site.urls)),
-
-)
+]
